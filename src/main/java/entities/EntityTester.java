da5fa5defb4c4768bd98ac1fd5f1fbbccad017e0
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-09-01 18:32:25 +0200

    add address entity

--- src/main/java/entities/EntityTester.java
+++ src/main/java/entities/EntityTester.java
@@ -13,14 +13,22 @@ public class EntityTester {
         Person p1 = new Person("Jonke", 1963);
         Person p2 = new Person("Blondie", 1959);
 
+        Address a1 = new Address("Store Torv 1", 2323, "Nr Snede");
+        Address a2 = new Address("Langgade 34", 1212, "Valby");
+
+        p1.setAddress(a1);
+        p2.setAddress(a2);
+
         em.getTransaction().begin();
         em.persist(p1);
         em.persist(p2);
-
         em.getTransaction().commit();
-        
+
         System.out.println("p1: " + p1.getName() + " " + p1.getId());
         System.out.println("p2: " + p2.getName() + " " + p2.getId());
+
+        System.out.println("Jonkes gade: " + p1.getAddress().getStreet());
+        System.out.println("Hvem bor på Store Torv 1: " + a1.getPerson().getName());
     }
 
 }
--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -1,10 +1,12 @@
 package entities;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Person implements Serializable {
@@ -15,6 +17,8 @@ public class Person implements Serializable {
     private Long id;
     private String name;
     private int year;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Address address;
 
     public Person() {
     }
@@ -39,4 +43,24 @@ public class Person implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+        if(address != null) {
+            address.setPerson(this);
+        }
+    }
+    
 }
